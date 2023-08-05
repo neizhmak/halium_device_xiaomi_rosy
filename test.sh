ae10f6634c5b7fbd2624aad3be5f039968b4b3b2
Artem Neizhmak <65288888+neizhmak@users.noreply.github.com>
2023-08-05 16:03:58 +0000

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
 if [ -z "${CI}" ] && ([ -e /.dockerenv ] || [ -e /run/.containerenv ]); then
-	IS_CONTAINER="true"
+    IS_CONTAINER="true"
+    echo "Ok"
+fi
+
+if [ -z "${CI}" ]; then
+    echo "Ok-0.5"
 fi
 
 if [ "${IS_CONTAINER}" == "true" ]; then
-	COMMIT=$(git rev-parse HEAD)
-  echo $COMMIT
+    COMMIT=$(git rev-parse HEAD)
+    echo $COMMIT
 else
-  echo "Bad"
+    echo "Bad"
 fi
